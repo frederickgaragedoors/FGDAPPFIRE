095a33eed6bd2c44a3de3887909c59400c75f14e
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-22 09:14:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa'; // UNCOMMENT FOR PWA DEPLOYMENT
+import { VitePWA } from 'vite-plugin-pwa'; // UNCOMMENT FOR PWA DEPLOYMENT
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
       plugins: [
         react(),
         // UNCOMMENT THE SECTION BELOW TO ENABLE PWA (Offline Support)
-        /*
+        
         VitePWA({
           registerType: 'autoUpdate',
           includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
@@ -40,7 +40,7 @@ export default defineConfig(({ mode }) => {
             ]
           }
         })
-        */
+        
       ],
       define: {
         // deeply polyfill process.env to prevent 'process is not defined' errors in some libs
