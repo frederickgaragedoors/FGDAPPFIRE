f7444504ecf5b8ac20a0719cfdf959cd6e31ae99
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-23 16:08:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa'; // UNCOMMENT FOR PWA DEPLOYMENT
+import { VitePWA } from 'vite-plugin-pwa'; // UNCOMMENT FOR PWA DEPLOYMENT
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
