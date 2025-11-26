80a867253434b81bc4e4fa8c127a35b6c1ee1006
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-26 09:16:00 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./contexts/**/*.{js,ts,jsx,tsx}",
-    "./hooks/**/*.{js,ts,jsx,tsx}",
-    "./services/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
